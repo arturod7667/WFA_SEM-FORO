cf60be65f9f8eb059cb8b18ab97a98c186acaba7
arturod7667 <91348773+arturod7667@users.noreply.github.com>
2022-01-19 22:25:28 -0300

    Add files via upload

---
+++ WFA_SEMAF/Semaforo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WFA_SEMAF
+{
+    class Semaforo
+    {
+        private byte estado;
+
+        public Semaforo()
+        {
+            // com rua1 = vd e rua2 = vm - situação inicial
+        }
+
+        public void setVerde(int rua)
+        {
+        }
+
+        public void setAmarelo(int rua)
+        {
+        }
+
+        public void setVermelho(int rua)
+        {
+        }
+
+        public byte getEstado()
+        {
+            return this.estado;
+        }
+
+        public int getEstado(int rua)
+        {
+            // 1. Vd - 2. Am - 3. Vm
+            byte aux = 0;
+            return aux;
+        }
+    }
+
+}
